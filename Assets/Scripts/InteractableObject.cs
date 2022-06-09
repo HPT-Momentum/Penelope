09d78d5be9dbe672836ff23150d4da329d258dcc
TimvdVooren <timvandervooren@gmail.com>
2022-06-09 23:02:41 +0200

    [Added] portals and website links

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -5,19 +5,19 @@ public abstract class InteractableObject : MonoBehaviour
     // set the key that is used to interact with the object
     public KeyCode key = KeyCode.E;
 
-	public enum InteractionType {
+    public enum InteractionType {
         Click,
         Hold
-	}
+    }
 
     float holdTime;
 
     public InteractionType interactionType;
 
     public abstract string GetDescription();
-    public abstract string OnInteract(GameObject dialogueBox);
+    public abstract string OnInteract(GameObject playerObject);
 
-	// methods for interaction type Hold
+    // methods for interaction type Hold
     public float GetHoldTime() => holdTime;
     public void IncreaseHoldTime() => holdTime += Time.deltaTime;
     public void ResetHoldTime() => holdTime = 0f;
