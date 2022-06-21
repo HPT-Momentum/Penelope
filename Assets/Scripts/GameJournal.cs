bdb32c3766f7d4ebee7a2348db079abd307b50fb
Max7oo <m.j.deruiter99@hotmail.com>
2022-06-21 14:35:08 +0200

    added comments

--- Assets/Scripts/GameJournal.cs
+++ Assets/Scripts/GameJournal.cs
@@ -9,26 +9,31 @@ public class GameJournal : MonoBehaviour
     public GameObject gameJournalMenu;
     public Text gameJournalText;
 
+    //Nieuwe list aanmaken
     List<string> journalLogs = new List<string>();
 
     public void addJournalLog (string log){
+        //Elke keer als een actiege logd wordt, voeg dan ook de tijd toe
         log+= DateTime.Now;
-
+        //Voeg de log toe aan de string
         journalLogs.Add(log);
     }
     
     public void OpenMenu(){
+        //Wanneer het menu wordt geopend, maak een leeg tekstvak aan en open gameJournalMenu
         gameJournalText.text = "";
         gameJournalMenu.SetActive(true);
-
+        //Reverse dan de list journalLogs
         var reverseList = new List<string>(journalLogs);
         reverseList.Reverse();
         foreach (string log in reverseList) {
+            //Vul gameJournalText.text met de log en \n zorgt ervoor dat na elke log een new line begint
             gameJournalText.text += log + "\n";
         }
     }
     
     public void CloseMenu(){
+        //Wanneer het menu wordt gesloten, sluit gameJournalMenu
         gameJournalMenu.SetActive(false);
     }
 }
